d98ff2ad44559c8efac475530431a2f86aabf823
Carlos Rodrigo <147435024+CarlosRodrigoInfo@users.noreply.github.com>
2023-10-17 22:58:12 -0300

    Add files via upload
    ultimos ajustes

--- js/MobileNavbar.js
+++ js/MobileNavbar.js
@@ -1,74 +1,68 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-        });
-    }
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+    });
+  }
 
-    handleClick() {
-        this.navList.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
 
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
 
 const mobileNavbar = new MobileNavbar(
-    "#burger",
-    ".nav-list",
-    ".nav-list li",
+  "#burger",
+  ".nav-list",
+  ".nav-list li",
 );
 mobileNavbar.init();
 
-const isAuthenticated = true; // Defina isso com base na autenticação do usuário
 
-if (isAuthenticated) {
-  const loginButton = document.querySelector(".login-btn");
-  loginButton.style.display = "inline-block"; // Mostrar o botão de perfil
-}
 
 const apiKey = '3ec3e5d091f9c442dc0688774f5594c4';
 const baseUrl = 'https://api.themoviedb.org/3';
 
 let maxTentativas = 25; // Defina o número máximo de tentativas
 let tentativas = 0; // Inicialize o contador de tentativas
-let currentPage = Math.floor(Math.random() * 1000) + 1; // Gere um número aleatório entre 1 e 10 para a página
+let currentPage = Math.floor(Math.random() * 100) + 1; // Gera um número aleatório entre 1 e 10 para a página
 
 // Função para buscar filmes ou séries aleatórias com base nos filtros
 function getRandomMedia() {
   // Obtenha os valores dos filtros do formulário
   const category = document.getElementById('cat').value;
   const mediaType = document.getElementById('mv').value;
-  const rating = '6.0'; // Avaliação fixa (você pode alterar conforme necessário)
+  const rating = '6.0'; // Avaliação fixa 
 
-  // Defina o tipo de mídia com base na seleção
+  // Define o tipo de mídia com base na seleção
   let searchMediaType;
   if (mediaType === 'filme') {
     searchMediaType = 'movie';
   } else if (mediaType === 'serie') {
     searchMediaType = 'tv';
   } else {
-    // Se a seleção não for válida, interrompa a função
     console.error('Tipo de mídia inválido.');
     return;
   }
@@ -88,7 +82,6 @@ function getRandomMedia() {
 
       // Verifique se a variável media é definida e se possui algum resultado
       if (media && media.length > 0) {
-        // Resto do seu código aqui
         const randomIndex = Math.floor(Math.random() * media.length);
         const selectedMedia = media[randomIndex];
 
@@ -143,7 +136,8 @@ document.addEventListener('DOMContentLoaded', function () {
   form.addEventListener('submit', function (event) {
     event.preventDefault();
     tentativas = 0; // Zera o contador de tentativas ao enviar o formulário
-    currentPage = Math.floor(Math.random() * 10) + 1; // Gere um número aleatório entre 1 e 10 para a página ao enviar o formulário
+    currentPage = Math.floor(Math.random() * 100) + 1; // Gera um número aleatório entre 1 e 1000 para a página ao enviar o formulário
     getRandomMedia();
   });
+  getRandomMedia();
 });
